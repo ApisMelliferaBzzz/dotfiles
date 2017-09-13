6b76bcf908cd5baf0d2286c272eca971544a2a44
Apis <apismelliferabzzz@gmail.com>
2017-09-14 00:37:23 +0300

    minor changes & fixes

--- dzenScripts/ewmhPanelLeft.sh
+++ dzenScripts/ewmhPanelLeft.sh
@@ -7,6 +7,27 @@ get_pager() {
 get_mpd() {
 	$PAT/music.sh
 }
+get_progress() {
+	$PAT/progress_bar.sh
+}
+get_bat() {
+	$PAT/battery.sh
+}
+get_cpu() {
+	$PAT/cpubar.sh
+}
+get_batIcon() {
+	$PAT/batIcon.sh
+}
+get_mem() {
+	$PAT/mem.sh
+}
+get_date() {
+	$PAT/date.sh
+}
+get_net() {
+	$PAT/internet.sh
+}
 # get_bat() {
 # 	/home/bee/.config/dzenScripts/battery.sh
 # }
@@ -23,12 +44,21 @@ get_mpd() {
 # 	/home/bee/.config/dzenScripts/date.sh
 # }
 while true; do
-	echo "^bg($BGD)^pa(0)^fg($IFG)^ib(0)^ca(1,mygtkmenu "$PAT"/LaunchMenuTop)^i("$PAT"/icons/arch_10x10_black.xpm) ^ca()^pa(22;0)$(get_pager)^pa(207;4)^bg($BGL)^fg($IFG)$(get_mpd) ^ca()^ca()^ca()^ca()^bg($BGL)"
+	echo -en "^bg($BGD)^pa(0;0)^fg($IFG)^ib(0)^ca(1,mygtkmenu "$PAT"/LaunchMenuTop)^i("$PAT"/icons/arch_10x10_black.xpm)"
+	echo -en " ^ca()^pa(22;0)$(get_pager)"
+	echo "^pa(207;4)^bg($BGL)^fg($IFG)$(get_mpd) ^ca()^ca()^ca()^ca()^bg($BGL)"
 	# echo -en "^pa(1368;6)^bg(#222)  ^fg($IFG)$(get_batIcon) " &
 	# echo -en "^pa(1394;5)^bg(#222)$(get_bat)   " &
 	# echo -en "^pa(1512;6)^bg(#222)^fg($IFG)^i(/home/bee/.dzen/icons/mem.xbm) ^p(;-1)$(get_mem)   " &
 	# echo -en "^pa(1644;6)^bg(#222)^fg($IFG)^i(/home/bee/.dzen/icons/cpu.xbm) ^p(;-1)$(get_cpu)  " &
 	# echo "^pa(1770;4)^fg($FG)^bg(#333)$(get_date)"
+#	echo -en "^pa(1352;6)^bg($BGD)  ^fg($IFG)$(get_batIcon)" &
+#	echo -en "^pa(1372;5)^bg($BGD) $(get_bat)   " &
+#	echo -en "^pa(1495;6)^bg($BGD)^fg($IFG)^i("$PAT"/icons/mem.xbm) ^p(;-1)$(get_mem)   " &
+#	echo -en "^pa(1627;6)^bg($BGD)^fg($IFG)^i("$PAT"/icons/cpu.xbm) ^p(;-1)$(get_cpu) " &
+#	echo -en "^pa(1747;6)^bg($BGD) $(get_net)  "
+#	echo "^pa(1773;4)^fg($FG)^bg($BGL)$(get_date)^pa(;0)^i($PAT/icons/black/tag.xpm) " #иконка - костыль для светлого фона после даты
+#	sleep 1
 	sleep 0.1
 	# wait;
 done
--- dzenScripts/startup.sh
+++ dzenScripts/startup.sh
@@ -5,6 +5,7 @@ source /home/bee/.config/dzenScripts/vars.sh
 killall dzen2
 
 sh $PAT/progress_bar.sh | dzen2 -x '220' -w '1125' -h '2' -ta 'l' -bg $BGL -y '0' -title-name 'progress_bar' -e 'onstart=lower' &
-#sleep is needed for progress_bar to be above main panel and at the same time beneath normal windows
-sleep 1 && sh $PAT/ewmhPanelLeft.sh | dzen2 -x '0' -y '0' -h '20' -w '1348' -ta l -title-name 'panel_left' -fn 'xos4 Terminus:pixelsize=12' -bg $BGL -e 'onstart=lower' &
+#sleep is needed for progress_bar to be above main panel and at the same time beneath maximized windows
+sleep 1 && sh $PAT/ewmhPanelLeft.sh | dzen2 -x '0' -y '0' -h '20' -w '1345' -ta l -title-name 'panel_left' -fn 'xos4 Terminus:pixelsize=12' -bg $BGL -e 'onstart=lower' &
 sh ~/.config/dzenScripts/ewmhPanelRight.sh | dzen2 -x '1345' -y '0' -h '20' -w '575' -ta l -bg $BGD -fn 'xos4 Terminus:pixelsize=12' -e 'onstart=lower' &
+#sleep 1 && sh $PAT/ewmhPanelUni.sh | dzen2 -x '0' -y '0' -h '20' -ta l -title-name 'panel_left' -fn 'xos4 Terminus:pixelsize=12' -bg $BGL -e 'onstart=lower' -dock
