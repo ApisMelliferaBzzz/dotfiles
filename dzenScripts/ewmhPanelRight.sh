d5c222f49e9ef550dbff068143fa8d226285b3a5
Apis Mellifera <apismelliferabzzz@gmail.com>
2017-09-22 01:43:41 +0300

    Changed battery to a volume bar

--- dzenScripts/ewmhPanelRight.sh
+++ dzenScripts/ewmhPanelRight.sh
@@ -16,6 +16,9 @@ get_mem() {
 get_date() {
 	$PAT/date.sh
 }
+get_vol() {
+	$PAT/volume.sh
+}
 get_net() {
 	$PAT/internet.sh
 }
@@ -23,10 +26,11 @@ while true; do
 	# echo -en "^bg(#222)^pa(0;5) ^fg($IFG)^ib(0)^ca(1,mygtkmenu /home/bee/.i3/LaunchMenuTop)^i(/home/bee/.dzen/icons/arch_10x10.xbm) ^ca()"
 	# echo -en "^pa(22;0)$(get_pager)" &
 	# echo -en "^pa(207;4)^bg(#333)^fg($IFG)$(get_mpd) ^ca()^ca()^ca()^ca()" &
-	echo -en "^pa(0;6)^bg($BGD)  ^fg($IFG)$(get_batIcon)" &
-	echo -en "^pa(21;5)^bg($BGD) $(get_bat)   " &
-	echo -en "^pa(146;6)^bg($BGD)^fg($IFG)^i("$PAT"/icons/mem.xbm) ^p(;-1)$(get_mem)   " &
-	echo -en "^pa(279;6)^bg($BGD)^fg($IFG)^i("$PAT"/icons/cpu.xbm) ^p(;-1)$(get_cpu)" &
+	# echo -en "^pa(0;6)^bg($BGD)  ^fg($IFG)$(get_batIcon)"
+	echo -en "^pa(0;6)^bg($BGD)  ^fg($IFG)$(get_vol)"
+	# echo -en "^pa(21;5)^bg($BGD) $(get_bat)   "
+	echo -en "^pa(146;6)^bg($BGD)^fg($IFG)^i("$PAT"/icons/mem.xbm) ^p(;-1)$(get_mem)   "
+	echo -en "^pa(279;6)^bg($BGD)^fg($IFG)^i("$PAT"/icons/cpu.xbm) ^p(;-1)$(get_cpu)"
 	echo -en "^pa(402;6)^bg($BGD) $(get_net)  "
 	echo "^pa(428;4)^fg($FG)^bg($BGL)$(get_date)^pa(;0)^i($PAT/icons/black/tag.xpm) " #иконка - костыль для светлого фона после даты
 	sleep 1
