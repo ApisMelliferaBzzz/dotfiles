ac42429a8de124eebe4fbd83609d8d0c15e98543
Apis Mellifera <apismelliferabzzz@gmail.com>
2017-09-28 00:15:27 +0300

    Started volume popup

---
+++ dzenScripts/volume_popup/volume_popup_handler.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+source /home/bee/.config/dzenScripts/vars.sh
+
+isMute=`amixer get Master | grep "Front Left:" | awk '{print $6}'`
+if [ $isMute == "[on]" ]; then
+    vol=`amixer get Master | grep "Front Left:" | awk '{print $5}' | tr -d '[]'`
+    if [ $vol == "0%" ]; then
+        echo -en "^fg(#303030)"
+    else
+        echo -en "^fg($IFG)"
+    fi
+else
+    echo -en "^fg(#303030)"
+fi
+
+res=$( eval echo $vol $GDBAR -h '14' );
+
+echo -en "^ca(4, amixer sset Master 10%+ )^ca(5, amixer sset Master 10%- )"
+echo -en "^bg(#404040)^ca(1, amixer sset Master 10%- )^i($PAT/icons/minus.xbm)^ca()^p(10;3)^bg(#303030)"
+echo -en $res
+echo "^p(10;-3)^bg(#404040)^ca(1, amixer sset Master 10%+ )^fg($IFG)^i($PAT/icons/plus.xbm)^ca()"
